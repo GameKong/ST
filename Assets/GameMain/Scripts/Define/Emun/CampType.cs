6b67810a77f1373c822785bf455e7e7712d89420
kongl <ling_kong@droidhang.com>
2022-02-19 18:30:51 +0800

    no message

---
+++ Assets/GameMain/Scripts/Define/Emun/CampType.cs
@@ -0,0 +1,25 @@
+﻿namespace ST
+{
+    /// <summary>
+    /// 阵营类型。
+    /// </summary>
+    public enum CampType : byte
+    {
+        Unknown = 0,
+
+        /// <summary>
+        /// 玩家阵营。
+        /// </summary>
+        Player,
+
+        /// <summary>
+        /// 敌人阵营。
+        /// </summary>
+        Enemy,
+
+        /// <summary>
+        /// 中立阵营。
+        /// </summary>
+        Neutral
+    }
+}
---
+++ Assets/GameMain/Scripts/Procedure/ProcedureExample.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using GameFramework;
+using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace ST
+{
+    public class ProcedureExample : ProcedureBase
+    {
+
+        public override bool UseNativeDialog
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+
+            string welcomeMessage = Utility.Text.Format("Hello! This is an empty project based on Game Framework {0}.", Version.GameFrameworkVersion);
+            Log.Info(welcomeMessage);
+            Log.Warning(welcomeMessage);
+            Log.Error(welcomeMessage);
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            
+        }
+    }
+}
